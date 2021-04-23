52a86f862108dbde0744f83133be312814852ad2
MosheBiran <73993888+MosheBiran@users.noreply.github.com>
2021-04-23 18:54:57 +0300

    123

--- app.js
+++ app.js
@@ -6,12 +6,79 @@ var pac_color;
 var start_time;
 var time_elapsed;
 var interval;
+var userDic={};
+userDic["k"]="k";
+userDic["u"]="u";
 
 $(document).ready(function() {
-	context = canvas.getContext("2d");
-	Start();
+	// context = canvas.getContext("2d");
+	// Start();
+
+	//register
+	$("#registerForm").validate({
+		rules: {
+			username: {
+				required: true
+			},
+			password: {
+				required: true,
+				minlength: 6,
+				validPassword: true
+			},
+			fullName: {
+				required: true,
+				validName: true
+			},
+			email: {
+				required: true,
+				email: true
+			}
+		},
+
+		messages: {
+			username: "Please enter username",
+			password: {
+				required: "Please enter a password",
+				minlength: "Password must consist at least 6 characters",
+				validPassword: "Please enter a valid password"
+			},
+			fullName: {
+				required: "Please enter your full name",
+				validName: "Name can only consist alphabetic chars"
+			},
+			email: {
+				required: "Please enter your Email",
+				email: "Please enter valid Email"
+			}
+		},
+
+		submitHandler: function() {
+			//add user to users dic
+
+			let username = document.getElementById("register_username").value;
+			let password = document.getElementById("register_password").value;
+
+			userDic[username] = password;
+			let form = $("#registerForm");
+			form[0].reset();
+			welcomeON();
+
+		}
+	});
 });
 
+// function submitHandlerregister() {
+// 	//add user to users dic
+
+// 	let username = document.getElementById("register_username").value;
+// 	let password = document.getElementById("register_password").value;
+
+// 	userDic[username] = password;
+// 	// let form = $("#registerForm");
+// 	// form[0].reset();
+// 	welcomeON();
+
+// }
 function Start() {
 	board = new Array();
 	score = 0;
@@ -82,6 +149,7 @@ function findRandomEmptyCell(board) {
 	return [i, j];
 }
 
+  
 function GetKeyPressed() {
 	if (keysDown[38]) {
 		return 1;
@@ -130,7 +198,24 @@ function Draw() {
 		}
 	}
 }
+function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+    var captionText = document.getElementById("caption");
+    captionText.innerHTML = element.alt;
+  }
+function DIV_none() {
+	var x = document.getElementById('DIV_none');
+	ar = document.getElementsByTagName("code");
+	for (i = 0; i < ar.length; ++i)
+   	ar[i].style.display = "none";
 
+	if (x.style.display === 'none') {
+	  x.style.display = 'block';
+	} else {
+	  x.style.display = 'none';
+	}
+  }
 function UpdatePosition() {
 	board[shape.i][shape.j] = 0;
 	var x = GetKeyPressed();
@@ -170,3 +255,153 @@ function UpdatePosition() {
 		Draw();
 	}
 }
+function resetElement() {
+	document.getElementById("welcome").style.display = "none";
+    document.getElementById("logIn").style.display = "none";
+	document.getElementById("signIn").style.display = "none";
+	document.getElementById("setting").style.display = "none";
+	document.getElementById("about").style.display = "none";
+	document.getElementById("UserScreen").style.display = "none";
+
+}
+function welcomeON() {
+resetElement()
+	document.getElementById("welcome").style.display = "block";
+}
+function logInON() {
+resetElement()
+	document.getElementById("logIn").style.display = "block";
+}
+function signInON() {
+resetElement()
+	document.getElementById("signIn").style.display = "block";
+}
+function settingON() {
+resetElement()
+	document.getElementById("setting").style.display = "block";
+}
+function aboutON() {
+resetElement()
+	document.getElementById("about").style.display = "block";
+}
+function UserScreenON() {
+resetElement()
+	document.getElementById("UserScreen").style.display = "block";
+}
+
+var logInmodal = document.getElementById('logIn');
+window.onclick = function(event) {
+	if (event.target == logInmodal) {
+		logInmodal.style.display = "none";
+		welcomeON()
+	}
+}
+var signInmodal = document.getElementById('signIn');
+window.onclick = function(event) {
+	if (event.target == signInmodal) {
+		signInmodal.style.display = "none";
+		welcomeON()
+	}
+}
+
+var settingmodal = document.getElementById('setting');
+window.onclick = function(event) {
+	if (event.target == settingmodal) {
+		settingmodal.style.display = "none";
+		welcomeON()
+	}
+}
+
+var aboutmodal = document.getElementById('about');
+window.onclick = function(event) {
+	if (event.target == aboutmodal) {
+		aboutmodal.style.display = "none";
+		welcomeON()
+	}
+}
+
+var UserScreenmodal = document.getElementById('UserScreen');
+window.onclick = function(event) {
+	if (event.target == UserScreenmodal) {
+		UserScreenmodal.style.display = "none";
+		welcomeON()
+	}
+}
+/*-------setVolume------- */
+var Volslider = document.getElementById("Volume");
+var Voloutput = document.getElementById("Volume_val");
+Voloutput.innerHTML = Volslider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+Volslider.oninput = function() {
+ Voloutput.innerHTML = this.value;
+}
+var x = document.getElementById("myAudio");
+
+function setVolume() { 
+  x.volume = Volslider.value/100;
+}
+
+var enemslider = document.getElementById("enemNum");
+var enemoutput = document.getElementById("enem_val");
+enemoutput.innerHTML = enemslider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+enemslider.oninput = function() {
+ enemoutput.innerHTML = this.value;
+}
+var lifeslider = document.getElementById("lifeNum");
+var lifeoutput = document.getElementById("life_val");
+lifeoutput.innerHTML = lifeslider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+lifeslider.oninput = function() {
+ lifeoutput.innerHTML = this.value;
+}
+
+
+
+
+
+
+
+
+$.validator.addMethod("validPassword", function(value) {
+    return /^(?=.[A-Za-z])(?=.\d)[A-Za-z\d]{6,}$/.test(value);
+});
+
+$.validator.addMethod("validName", function(value) {
+    return /^[a-zA-Z ]+$/.test(value);
+});
+
+$.validator.addMethod("passwordMatch", function() {
+    let username = $('#uname').val();
+    let password = $('#pass').val();
+
+    let validUserName = "";
+    let validPassword = "";
+    for (var i = 0; i < users.length; i++) {
+        if (users[i][0] == username) {
+            validUserName = users[i][0];
+
+            if (users[i][1] == password) {
+                validPassword = users[i][1];
+                break;
+            }
+        }
+    }
+    if (validUserName != "") {
+        if (password == "") {
+            return false;
+
+        } else if (validPassword == "") {
+            return false;
+
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
+});
+
