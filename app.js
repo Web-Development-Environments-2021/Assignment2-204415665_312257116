fb0ebf44029e9158ec134270f8de0b65cc64f38c
DanielDinshtein <danieldinsh@gmail.com>
2021-04-28 14:34:08 +0300

    After Fix Eye

--- app.js
+++ app.js
@@ -274,7 +274,7 @@ function Draw() {
 					context.fillStyle = pac_color; //color
 					context.fill();
 					context.beginPath();
-					context.arc(center.x + 7.5, center.y - 5, 2.5, 0, 2 * Math.PI); // circle
+					context.arc(center.x + 7.5, center.y - 2.5, 2.5, 0, 2 * Math.PI); // circle
 					context.fillStyle = "black"; //color
 					context.fill();
 				} else if (pac_dir == "down"){
@@ -284,7 +284,7 @@ function Draw() {
 					context.fillStyle = pac_color; //color
 					context.fill();
 					context.beginPath();
-					context.arc(center.x + 7.5, center.y - 5, 2.5, 0, 2 * Math.PI); // circle
+					context.arc(center.x + 7.5, center.y - 2.5, 2.5, 0, 2 * Math.PI); // circle
 					context.fillStyle = "black"; //color
 					context.fill();
 				} else if (pac_dir == "left"){
